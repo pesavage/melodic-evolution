01f897f84d5e0f2d32f0c754ad1659493297ed12
Sam Passmore <passmore.sam@gmail.com>
2021-11-05 14:37:56 +1300

    install fix

--- install.R
+++ install.R
@@ -2,45 +2,39 @@
 # These will depend on the R version. Please check sessionInfo.txt
 
 
-if (!requireNamespace("pacman", quietly = TRUE)){
-  install.packages("pacman")
+if (!requireNamespace(pacman, quietly = TRUE)){
+  install.packages(pacman)
 }
 library(pacman)
 
-list.of.packages <- c("plotrix", 
-                      "seqinr",
-                      "Biostrings",
-                      "seriation",
-                      "tidyr",
-                      "ggplot2",
-                      "dplyr",
-                      "varhandle",
-                      "stringr",
-                      "seqRFLP",
-                      "pwr",
-                      "lsr",
-                      "sp",
-                      "RColorBrewer",
-                      "phangorn",
-                      "GADMTools",
-                      "BiocManager",
-                      "ggthemes",
-                      "boot",
-                      "dplyr",
-                      "ggthemes",
-                      "readxl",
-                      "brms",
-                      "tidybayes",
-                      "bayesplot",
-                      "projpred",
-                      "assertthat",
-                      # for maps
-                      "patchwork",
-                      "sf",
-                      "rnaturalearth"
-                      )
-
-
-p_install(list.of.packages, 
-          character.only = TRUE, 
-          force = FALSE)
+pacman::p_load(plotrix, 
+          seqinr,
+          Biostrings,
+          seriation,
+          tidyr,
+          ggplot2,
+          dplyr,
+          varhandle,
+          stringr,
+          seqRFLP,
+          pwr,
+          lsr,
+          sp,
+          RColorBrewer,
+          phangorn,
+          GADMTools,
+          BiocManager,
+          ggthemes,
+          boot,
+          dplyr,
+          ggthemes,
+          readxl,
+          brms,
+          tidybayes,
+          bayesplot,
+          projpred,
+          assertthat,
+          # for maps
+          patchwork,
+          sf,
+          rnaturalearth)
